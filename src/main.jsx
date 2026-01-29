9891e320d717975a8098f2b67daa1896b58b6ae7
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-29 22:00:21 +0600

    register

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,21 @@ import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Root from './components/layouts/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import login from './components/login/login.jsx';
+import Register from './components/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    children: [
+      {index:true, Component: Home},
+      {path: "login", Component: login},
+      {path: "register", Component: Register}
+    ]
+
+   
   },
 ]);
 createRoot(document.getElementById('root')).render(
